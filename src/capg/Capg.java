d8194d4a5ea5f1f1306d22c7e7ac9af88b7daa05
Priyanshu Rohilla <priyanshurohillaverma@gmail.com>
2022-03-02 14:16:11 +0530

    Initial commit

---
+++ src/capg/Capg.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capg;
+
+/**
+ *
+ * @author priya
+ */
+class Address{
+ String city;
+ public Address(String city){ this.city = city; }
+}
+
+public class Customer {
+ int cid;
+ Address addr;
+ 
+ public Customer( int cid ){ this.cid = cid; }
+ public void setAddress(Address addr){ this.addr = addr;}
+
+ public static void main(String[] args) {
+  Address a1 = new Address("Bangalore");
+  Customer c2 = new Customer(101);
+  System.out.print(c2.cid + c2.addr.city);
+ }
+}
